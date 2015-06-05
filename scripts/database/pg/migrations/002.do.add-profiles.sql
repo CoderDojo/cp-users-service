784be1eb96cfec900e3d0f5cf6e931a2421d2fac
sigismondm <sigismond.mclaughlin@nearform.com>
2015-06-05 15:12:48 -0500

    Initial profile table

--- scripts/database/pg/migrations/002.do.add-profiles.sql
+++ scripts/database/pg/migrations/002.do.add-profiles.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS cd_parentguardianprofiles;
+DROP TABLE IF EXISTS cd_profiles;
 
-CREATE TABLE cd_parentguardianprofiles
+CREATE TABLE cd_profiles
 (
   id character varying,
   parent_guardian_id character varying,
@@ -8,26 +8,8 @@ CREATE TABLE cd_parentguardianprofiles
   dob date,
   gender character varying,
   email character varying NOT NULL,
-  children character varying [],
   phone character varying,
   CONSTRAINT pk_parent_guardian_profile_id PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
-);
-
-DROP TABLE IF EXISTS youth_profiles;
-
-CREATE TABLE cd_youthprofiles
-(
-  id character varying,
-  youth_id character varying,
-  name character varying,
-  dob date,
-  gender character varying,
-  email character varying,
-  parents character varying [],
-  CONSTRAINT pk_youth_profile_id PRIMARY KEY (id)
 )
 WITH (
   OIDS=FALSE
