6a80b7cdc2a3e71822d84e97e60c0d06f4a4a31e
sigismondm <sigismond.mclaughlin@nearform.com>
2015-06-08 20:29:10 -0500

    WIP - Update db schema

--- scripts/database/pg/migrations/002.do.add-profiles.sql
+++ scripts/database/pg/migrations/002.do.add-profiles.sql
@@ -3,13 +3,22 @@ DROP TABLE IF EXISTS cd_profiles;
 CREATE TABLE cd_profiles
 (
   id character varying,
-  parent_guardian_id character varying,
-  name character varying,
+  user_id character varying,
+  alias character varying,
   dob date,
-  gender character varying,
-  email character varying NOT NULL,
-  phone character varying,
-  CONSTRAINT pk_parent_guardian_profile_id PRIMARY KEY (id)
+  country character varying,
+  address character varying,
+  last_editied timestamp,
+  email charachter varying,
+  phone charachter varying,
+  parents text[],
+  children text[],
+  linkedin charachter varying,
+  twitter charachter varying,
+  languages text[],
+  programming_languages text[],
+  notes charachter varying,
+  projects text[]
 )
 WITH (
   OIDS=FALSE
