7f29f3a52b1110e881807500bf357a6a88466f23
david-cahill <davidrichardcahill@gmail.com>
2015-06-10 15:25:11 +0100

    add profile related actions

--- profiles.js
+++ profiles.js
@@ -8,6 +8,7 @@ module.exports = function(options) {
 
   seneca.add({role: plugin, cmd: 'create'}, cmd_create);
   seneca.add({role: plugin, cmd: 'list'}, cmd_list);
+  seneca.add({role: plugin, cmd: 'save'}, cmd_save);
 
   function cmd_create(args, done){
     var profile = args.profile;
@@ -18,8 +19,14 @@ module.exports = function(options) {
 
   function cmd_list(args, done){
     var query = args.query;
-
+    
     seneca.make$(PARENT_GUARDIAN_PROFILE_ENTITY).list$(query, done);
+  }
+
+  function cmd_save(args, done) {
+    var profile = args.profile;
+
+    seneca.make$(PARENT_GUARDIAN_PROFILE_ENTITY).save$(profile, done);
   }
 
   return {
