f7c34413d818f8ba93d46e81f46091fa09983ccc
Damian Beresford <dberesford@gmail.com>
2015-07-23 14:23:54 +0100

    Fixed postgres env vars

--- scripts/load_test_data.sh
+++ scripts/load_test_data.sh
@@ -7,7 +7,7 @@ USAGE="Usage: ./$SCRIPT <config>"
 source "$PROJECT_DIR/scripts/exec_on_env.sh"
 
 function postgres_test_data {
-    psql --single-transaction -h $PG_HOST -U $POSTGRES_USERNAME -d $POSTGRES_NAME -f "$PROJECT_DIR/scripts/database/pg/populate-users.sql" --port $PG_PORT
+    psql --single-transaction -h $POSTGRES_HOST -U $POSTGRES_USERNAME -d $POSTGRES_NAME -f "$PROJECT_DIR/scripts/database/pg/populate-users.sql" --port $POSTGRES_PORT
 }
 
 function delete_elasticsearch_index {
