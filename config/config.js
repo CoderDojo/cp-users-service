dc2a6f11f446598c4374afabc1bf7abbc12db627
Damian Beresford <dberesford@gmail.com>
2015-07-30 11:55:09 +0100

    Send seneca debug messages directly to logentries

--- config/config.js
+++ config/config.js
@@ -1,7 +1,47 @@
 'use strict';
 var path = require('path');
+var assert = require('assert');
+var LogEntries = require('le_node');
 
 module.exports = function() {
+  function log () {
+    // seneca custom log handlers
+    function debugHandler() {
+      if (process.env.LOGENTRIES_ENABLED === 'true') {
+        assert.ok(process.env.LOGENTRIES_DEBUG_TOKEN, 'No LOGENTRIES_DEBUG_TOKEN set');
+        var le = new LogEntries({
+          token: process.env.LOGENTRIES_DEBUG_TOKEN,
+          flatten: true,
+          flattenArrays: true
+        });
+
+        le.log('debug', arguments);
+      }
+    }
+
+    function errorHandler() {
+      console.error(JSON.stringify(arguments));
+
+      if (process.env.LOGENTRIES_ENABLED === 'true') {
+        assert.ok(process.env.LOGENTRIES_ERRORS_TOKEN, 'No LOGENTRIES_ERROR_TOKEN set');
+        var le = new LogEntries({
+          token: process.env.LOGENTRIES_ERRORS_TOKEN,
+          flatten: true,
+          flattenArrays: true
+        });
+
+        le.log('err', arguments);
+      }
+    }
+
+    return {
+      map:[{
+        level:'debug', handler: debugHandler
+      }, {
+        level:'error', handler: errorHandler
+      }]
+    };
+  };
 
   function pgConfig() {
     return {
@@ -81,6 +121,7 @@ module.exports = function() {
       clients: {
         coderdojoadultforums: process.env.CODERDOJO_FORUMS_SECRET || 'ilikecode'
       }
-    }
+    },
+    log: log()
   };
 }
