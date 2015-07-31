e15d8c34ee7a34223059eefaa274122872894ed2
sigismondm <sigismond.mclaughlin@nearform.com>
2015-07-30 20:23:29 -0500

    Remove user object when registering

--- users.js
+++ users.js
@@ -135,7 +135,7 @@ module.exports = function(options){
     }
 
     function registerUser(success, done){
-      args = _.omit(args, ['g-recaptcha-response', 'zenHostname', 'locality']);
+      args = _.omit(args, ['g-recaptcha-response', 'zenHostname', 'locality', 'user']);
 
       args.roles = ['basic-user'];
       args.mailingList = (args.mailingList) ? 1 : 0;
