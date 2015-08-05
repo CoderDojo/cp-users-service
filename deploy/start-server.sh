e98fceecc6abe629cefe1e2561237701d616d9a4
Damian Beresford <dberesford@gmail.com>
2015-08-05 22:47:50 +0100

    Stop app before starting in start.sh

--- deploy/start-server.sh
+++ deploy/start-server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+isExistApp=`ps -eaf |grep cp-users-service |grep -v grep| awk '{ print $2; }'`
+if [[ -n $isExistApp ]]; then
+    service cp-users-service stop
+fi
+
 service cp-users-service start
