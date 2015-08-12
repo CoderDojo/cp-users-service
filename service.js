808df88e96867315bf631f3025e41377bbb4d69e
Damian Beresford <dberesford@gmail.com>
2015-08-12 12:32:29 +0100

    workaround for event emitter warning leak

--- service.js
+++ service.js
@@ -1,6 +1,8 @@
 'use strict';
 
 if (process.env.NEW_RELIC_ENABLED === "true") require('newrelic');
+process.setMaxListeners(0);
+require('events').EventEmitter.prototype._maxListeners = 100;
 
 var _ =require('lodash');
 var config = require('./config/config.js')();
