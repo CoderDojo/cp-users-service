494cf6ecf1197399e92aca166fb35851c8232839
david-cahill <davidrichardcahill@gmail.com>
2015-08-12 15:30:47 +0100

    fix locality

--- email-notifications.js
+++ email-notifications.js
@@ -3,7 +3,8 @@
 module.exports = function( options ) {
   var seneca = this;
   var plugin = 'email-notifications';
-
+  var fs = require('fs');
+  var path = require('path');
   var so = seneca.options();
 
   options = seneca.util.deepextend({
@@ -15,6 +16,9 @@ module.exports = function( options ) {
   function send_notification(args, done) {
     if (options.sendemail && options.email) {
       var email = options.email[args.code];
+      if (!fs.existsSync(path.join(__dirname, '/email-templates/', args.code))) {
+        args.code = args.code.substring(0, args.code.length-5) + 'en_US';
+      }
       seneca.act({
         role: 'mail', cmd: 'send',
         code: args.code,
