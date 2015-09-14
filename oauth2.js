05b60675c1debae8bed03602516a0d01957ffdaf
Cristian Kiss <cristian.kiss@nearform.com>
2015-09-15 01:35:24 +0300

    npm lint and linting fixes

--- oauth2.js
+++ oauth2.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var async = require('async');
 var cuid = require('cuid');
 
-module.exports = function(options){
+module.exports = function (options) {
   var seneca = this;
   var plugin = 'cd-oauth2';
   var OAUTH2_ENTITY = 'cd/oauth2';
@@ -14,15 +14,15 @@ module.exports = function(options){
   seneca.add({role: plugin, cmd: 'profile'}, cmd_profile);
 
   // Note: currently got from the config, this may be moved to the database in the near future..
-  function _verifyClientId(clientId, cb) {
-    setImmediate(function() {
+  function _verifyClientId (clientId, cb) {
+    setImmediate(function () {
       if (!options.clients[clientId]) return cb('Invalid client_id: ' + clientId);
       return cb();
     });
-  };
+  }
 
-  function _getAccessCodeForUser(user, cb) {
-    seneca.make$(OAUTH2_ENTITY).list$({userid: user.id}, function(err, auths){
+  function _getAccessCodeForUser (user, cb) {
+    seneca.make$(OAUTH2_ENTITY).list$({userid: user.id}, function (err, auths) {
       if (err) return cb(err);
       if (auths.length > 0) return cb(null, auths[0].code);
 
@@ -32,52 +32,51 @@ module.exports = function(options){
       ucEnt.code = code;
       ucEnt.token = cuid();
       ucEnt.created = new Date();
-      ucEnt.save$(function(err) {
+      ucEnt.save$(function (err) {
         if (err) return cb(err);
         return cb(null, code);
       });
     });
-  };
+  }
 
-  function _getAccessTokenForAccessCode(code, cb) {
-    seneca.make$(OAUTH2_ENTITY).list$({code:code}, function(err, auths){
+  function _getAccessTokenForAccessCode (code, cb) {
+    seneca.make$(OAUTH2_ENTITY).list$({code: code}, function (err, auths) {
       if (err) return cb(err);
       if (auths.length === 0) return cb('No token found for code: ' + code);
       return cb(null, auths[0].token);
     });
-  };
+  }
 
-  function _getUserForAccessToken(token, cb) {
-
+  function _getUserForAccessToken (token, cb) {
     async.waterfall([
       getToken,
       getUser,
       checkPermissions
     ], cb);
 
-    function getToken(done) {
-      seneca.make$(OAUTH2_ENTITY).list$({token:token}, function(err, auths){
+    function getToken (done) {
+      seneca.make$(OAUTH2_ENTITY).list$({token: token}, function (err, auths) {
         if (err) return done(err);
         if (auths.length === 0) return done('No user found for token: ' + token);
         return done(null, auths);
       });
     }
 
-    function getUser(auths, done) {
-      seneca.act({role: 'cd-users', cmd:'load', id: auths[0].userid}, done);
+    function getUser (auths, done) {
+      seneca.act({role: 'cd-users', cmd: 'load', id: auths[0].userid}, done);
     }
 
-    function checkPermissions(user, done) {
-      seneca.act({role: 'cd-profiles', cmd: 'list', query:{userId: user.id}}, function (err, profiles) {
-        if(err) return done(err);
+    function checkPermissions (user, done) {
+      seneca.act({role: 'cd-profiles', cmd: 'list', query: {userId: user.id}}, function (err, profiles) {
+        if (err) return done(err);
         var userProfile = profiles[0];
         user.profileId = userProfile.id;
-        if(userProfile.userType === 'champion') user.isChampion = true;
-        if(userProfile.userType === 'attendee-o13') user.isYouthOver13 = true;
-        if(userProfile.userType === 'mentor') user.isMentor = true;
+        if (userProfile.userType === 'champion') user.isChampion = true;
+        if (userProfile.userType === 'attendee-o13') user.isYouthOver13 = true;
+        if (userProfile.userType === 'mentor') user.isMentor = true;
 
-        seneca.act({role: 'cd-dojos', cmd: 'load_usersdojos', query:{userId: user.id}}, function (err, usersDojos) {
-          if(err) return done(err);
+        seneca.act({role: 'cd-dojos', cmd: 'load_usersdojos', query: {userId: user.id}}, function (err, usersDojos) {
+          if (err) return done(err);
           var championTypeFound = _.find(usersDojos, function (userDojo) {
             return _.contains(userDojo.userTypes, 'champion');
           });
@@ -87,58 +86,56 @@ module.exports = function(options){
           var mentorTypeFound = _.find(usersDojos, function (userDojo) {
             return _.contains(userDojo.userTypes, 'mentor');
           });
-          if(championTypeFound) user.isChampion = true;
-          if(youthOver13TypeFound) user.isYouthOver13 = true;
-          if(mentorTypeFound) user.isMentor = true;
+          if (championTypeFound) user.isChampion = true;
+          if (youthOver13TypeFound) user.isYouthOver13 = true;
+          if (mentorTypeFound) user.isMentor = true;
           return done(null, user);
         });
-
       });
     }
-    
-  };
+  }
 
-  function cmd_authorize(args, done) {
+  function cmd_authorize (args, done) {
     if (args.response_type !== 'code') {
       return done(null, {error: 'Only authorization code auth supported!'});
     }
 
-    _verifyClientId(args.client_id, function(err) {
+    _verifyClientId(args.client_id, function (err) {
       if (err) return done(null, {error: err});
 
       if (!args.user) {
         return done(null, {
-          http$:{
+          http$: {
             redirect: '/login?redirect=' + args['redirect_uri']
           }
         });
       }
 
-      _getAccessCodeForUser(args.user, function(err, code) {
+      _getAccessCodeForUser(args.user, function (err, code) {
         if (err) return done(null, {error: err, http$: {status: 500}});
 
         done(null, {
-          http$:{
+          http$: {
             redirect: args['redirect_uri'] + '?code=' + code
           }
         });
       });
     });
-  };
+  }
 
-  function cmd_token(args, done) {
-    _getAccessTokenForAccessCode(args.code, function(err, access_token) {
+  function cmd_token (args, done) {
+    _getAccessTokenForAccessCode(args.code, function (err, access_token) {
       if (err) return done(null, {error: err, http$: {status: 500}});
 
       var resp = {
-        'access_token': access_token,
+        'access_token': access_token
       };
       return done(null, resp);
     });
-  };
+  }
 
-  function cmd_profile(args, done) {
-    _getUserForAccessToken(args.access_token, function(err, user) {
+  function cmd_profile (args, done) {
+    _getUserForAccessToken(args.access_token, function (err, user) {
       if (err) return done(null, {error: err, http$: {status: 500}});
       var profile = {
         id: user.id,
@@ -152,10 +149,9 @@ module.exports = function(options){
       };
       return done(null, profile);
     });
-  };
+  }
 
   return {
     name: plugin
   };
-
 };
