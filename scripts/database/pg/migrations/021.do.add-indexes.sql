43ca498ddc13f7d123689cf3c72e66531079741c
ante-sabo <ante@192.168.3.226>
2016-10-20 18:06:11 +0100

    972 wrong exception handling

--- scripts/database/pg/migrations/021.do.add-indexes.sql
+++ scripts/database/pg/migrations/021.do.add-indexes.sql
@@ -3,12 +3,12 @@ DO $$
         BEGIN
             CREATE INDEX users_lower_email ON sys_user(lower(email));
         EXCEPTION
-            WHEN duplicate_column THEN RAISE NOTICE 'index users_lower_email already exists in sys_user.';
+           RAISE NOTICE 'problem creating index users_lower_email in sys_user.';
         END;
         BEGIN
             CREATE INDEX users_username ON sys_user(username);
         EXCEPTION
-            WHEN duplicate_column THEN RAISE NOTICE 'index users_username already exists in sys_user.';
+            RAISE NOTICE 'problem creating index users_username in sys_user.';
         END;
     END;
 $$
