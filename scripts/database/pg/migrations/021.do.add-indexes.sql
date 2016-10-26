981e27b6572449573bf573ffd495ebd75ccbb13c
ante-sabo <ante@192.168.3.226>
2016-10-26 09:57:18 +0100

    927 added missing  WHEN OTHERS THEN

--- scripts/database/pg/migrations/021.do.add-indexes.sql
+++ scripts/database/pg/migrations/021.do.add-indexes.sql
@@ -2,12 +2,12 @@ DO $$
     BEGIN
         BEGIN
             CREATE INDEX users_lower_email ON sys_user(lower(email));
-        EXCEPTION
+        EXCEPTION WHEN OTHERS THEN
            RAISE NOTICE 'problem creating index users_lower_email in sys_user.';
         END;
         BEGIN
             CREATE INDEX users_username ON sys_user(username);
-        EXCEPTION
+        EXCEPTION WHEN OTHERS THEN
             RAISE NOTICE 'problem creating index users_username in sys_user.';
         END;
     END;
