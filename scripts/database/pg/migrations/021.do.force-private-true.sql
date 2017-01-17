ea5ad03cd34556af60973719c3f1dd51623d2344
Wardormeur <wardormeur@gmail.com>
2017-01-17 17:34:44 +0000

    Force private to non-null

--- scripts/database/pg/migrations/021.do.force-private-true.sql
+++ scripts/database/pg/migrations/021.do.force-private-true.sql
@@ -3,5 +3,8 @@ DO $$
         BEGIN
           UPDATE cd_profiles SET private = true WHERE private IS NULL;
         END;
+        BEGIN
+          ALTER TABLE cd_profiles ALTER COLUMN private SET NOT NULL;
+        END;
     END;
 $$
