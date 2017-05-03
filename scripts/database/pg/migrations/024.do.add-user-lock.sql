6d34741d7e071bd30cd2ddb259887900fb55d6cd
butlerx <cianbutlerx@gmail.com>
2017-05-03 12:42:48 +0100

    update to seneca user 2

--- scripts/database/pg/migrations/024.do.add-user-lock.sql
+++ scripts/database/pg/migrations/024.do.add-user-lock.sql
@@ -1,7 +1,7 @@
 DO $$
     BEGIN
         BEGIN
-            ALTER TABLE sys_user ADD COLUMN lock_try integer;
+            ALTER TABLE sys_user ADD COLUMN failed_login_count integer;
         EXCEPTION
             WHEN duplicate_column THEN RAISE NOTICE 'column token already exists in sys_user.';
         END;
