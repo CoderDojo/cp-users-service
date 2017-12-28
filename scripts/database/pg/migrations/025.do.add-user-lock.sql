88586d1a5078a13ed5ce26bd7b57a59394753cfe
butlerx <butlerx@notthe.cloud>
2017-12-28 14:03:49 +0000

    fix errors

--- scripts/database/pg/migrations/025.do.add-user-lock.sql
+++ scripts/database/pg/migrations/025.do.add-user-lock.sql
@@ -3,7 +3,7 @@ DO $$
         BEGIN
             ALTER TABLE sys_user ADD COLUMN failed_login_count integer;
         EXCEPTION
-            WHEN duplicate_column THEN RAISE NOTICE 'column token already exists in sys_user.';
+            WHEN duplicate_column THEN RAISE NOTICE 'column failed_login_count already exists in sys_user.';
         END;
     END;
 $$
