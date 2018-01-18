2ba9f917e75d83a35d71710fdeff877b831e61ed
butlerx <butlerx@notthe.cloud>
2018-01-18 22:19:06 +0000

    objoscate sql

--- newrelic.js
+++ newrelic.js
@@ -10,8 +10,11 @@
 exports.config = {
   app_name: ['cp-users-service'],
   agent_enabled: false, // set via NEW_RELIC_ENABLED for production
-  license_key: '',      // set via NEW_RELIC_LICENSE_KEY
+  license_key: '', // set via NEW_RELIC_LICENSE_KEY
   filepath: '/tmp/newrelic_agent_users.log',
+  transaction_tracer: {
+    record_sql: 'obfuscated'
+  },
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
