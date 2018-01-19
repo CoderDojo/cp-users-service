3bcd307b2f778bf3a6479ae4435dbf8bbbc9bdcc
Cian Butler <butlerx@notthe.cloud>
2018-01-19 10:36:39 +0000

    add seneca new-relic (#285)
    * add seneca new-relic

--- newrelic.js
+++ newrelic.js
@@ -10,6 +10,17 @@
 exports.config = {
   app_name: ['cp-users-service'],
   agent_enabled: false, // set via NEW_RELIC_ENABLED for production
-  license_key: '',      // set via NEW_RELIC_LICENSE_KEY
-  filepath: '/tmp/newrelic_agent_users.log'
+  license_key: '', // set via NEW_RELIC_LICENSE_KEY
+  filepath: '/tmp/newrelic_agent_users.log',
+  transaction_tracer: {
+    record_sql: 'obfuscated'
+  },
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
 };
