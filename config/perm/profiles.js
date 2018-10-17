f72eec947f2f87ac271cace2e54767022cdab1cc
Wardormeur <wardormeur@gmail.com>
2018-10-17 15:16:16 +0100

    Ensure perms for saving a profile is self register doesnt use this endpoint if you're concerned

--- config/perm/profiles.js
+++ config/perm/profiles.js
@@ -4,6 +4,16 @@ module.exports = function(){
   return {
       'create': [{
         role: 'basic-user',
+        customValidator: [{
+          role: 'cd-users',
+          cmd: 'is_self'
+        }]
+      }, {
+        role: 'basic-user',
+        customValidator: [{
+          role: 'cd-users',
+          cmd: 'is_parent_of'
+        }]
       }],
       'user_profile_data': [{
         role: 'none',
